a87e1172e1acc57849cdcee264af49d9b627a7d8
chiraggandhi123 <chiraggandhi70726@gmail.com>
2019-06-10 12:15:18 +0530

    second

---
+++ book.py
@@ -0,0 +1,12 @@
+book={
+    "name":"learning Python",
+    "page":320,
+    "authors":[
+        {"name":"A1",
+        "age":30
+        },{"name":"A2",
+           "age":40
+                
+        }
+        ]
+}
